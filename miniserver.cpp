65c3f2af3c4787586f2cd983684d4ce08ac9d6cc
Ayoub Ogbi <aogbi@c3r6p7.1337.ma>
2025-07-10 13:46:58 +0100

    start the project

---
+++ miniserver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <errno.h>
+#include <cstring>      // For memset
+#include <unistd.h>     // For close()
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <arpa/inet.h>  // For inet_addr
+
+#define PORT 8080
+
+int main() {
+    
+    return 0;
+}
